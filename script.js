33474354fba195db85034f2540cfa3fca6b264da
Aarya <amathrej@asu.edu>
2022-05-11 01:32:41 -0700

    Add some intro level javascript code into script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('Hello');
+console.log('World!');
+
+// comments are made with //
+
+let currentUserName = 'Aarya'; //we use camelCase for variables
+const BIRTHDAY = '04.01.2002'; //we name constant as UpperCase
+
+// Variables 
+
+alert(BIRTHDAY); //alert prints a variable value
+
+console.log(500 + 250);
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+console.log((4+6+9)/ 77);
+
+let a = 10;
+console.log(a);
+console.log(9*a);
+let b = 7 * a;
+console.log(b);
+
+const MAX = 57;
+let actual = MAX -13;
+let percentage = actual/MAX;
